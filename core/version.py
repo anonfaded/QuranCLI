1ef51584ebe3f66703eeab8a9a9bdf56cf8637fa
Faded <anonfaded@pm.me>
2025-05-22 05:08:55 +0500

    chore: bump version to 1.3.0 and update version code to 4

--- core/version.py
+++ core/version.py
@@ -1,3 +1,3 @@
 # core/version.py
-VERSION = "1.2.0"  # Semantic versioning for comparisons with github tags
-VERSION_CODE = 3 # Integer version code
+VERSION = "1.3.0"  # Semantic versioning for comparisons with github tags
+VERSION_CODE = 4 # Integer version code
